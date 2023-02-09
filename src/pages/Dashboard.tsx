9f6621b228d28273886c40c6777769ddf45ce0b9
andhika001 <andhikadwi980@gmail.com>
2023-02-09 07:35:23 +0700

    netlify change

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -42,9 +42,7 @@ const Dashboard = () => {
 
   const refreshToken = async () => {
     try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_BASE_URL}/api/token`
-      );
+      const response = await axios.get(`/api/token`);
       const decoded: any = jwt_decode(response.data.accessToken);
       setId(decoded._id);
       setName(decoded.name);
@@ -58,9 +56,7 @@ const Dashboard = () => {
 
   const getTransactions = async () => {
     try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_BASE_URL}/api/transactions`
-      );
+      const response = await axios.get(`/api/transactions`);
       const income = response.data.filter(
         (t: any) =>
           t.type === 'income' && t.date.substring(0, 10) === formattedToday
